442918fc81b186ed95c249074648cabe86258825
Kunihiko Miyanaga <miyanaga@ideamans.com>
2023-11-07 02:10:50 +0900

    $(function() {...})の時間のかかる処理をsetTimeoutで非同期に

--- inventory/get/https/www.gov-online.go.jp/js/common_2021.js
+++ inventory/get/https/www.gov-online.go.jp/js/common_2021.js
@@ -1,10 +1,14 @@
 // 別ウィンドウのa要素内を<span class="tBlank_inner">で囲む
 $(function () {
-  $('a[target="_blank"]').wrapInner('<span class="tBlank_inner"></span>');
+  setTimeout(function () {
+    $('a[target="_blank"]').wrapInner('<span class="tBlank_inner"></span>');
+  });
 });
 // 別ウィンドウのa要素内emの直前にアイコンとalt情報を機械的に設置する
 $(function () {
-  $('a[target="_blank"] .tBlank_inner').after(
-    '<img src="/module/common/images/mark008.svg" class="tBlank" alt="別ウインドウで開きます">'
-  );
+  setTimeout(function () {
+    $('a[target="_blank"] .tBlank_inner').after(
+      '<img src="/module/common/images/mark008.svg" class="tBlank" alt="別ウインドウで開きます">'
+    );
+  });
 });
